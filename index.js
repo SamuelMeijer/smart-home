0ec30f899aa4832812335dec21243da3b75c7ba9
Samuel Meijer <Samuel.j.meijer@gmail.com>
2021-04-29 14:59:48 +0200

    Adding handling of Lights

--- index.js
+++ index.js
@@ -126,6 +126,52 @@ app.get('/api/devices/camera/:id', (req, res) => {
     };
 });
 
+/* LIGHTS 
+Path: /api/decivces/light/:id
+Queries: power=VALUE (on/off) , color=VALUE ('colorcode') , brightness=VALUE (0-1)
+*/
+app.get('/api/devices/light/:id', (req, res) => {
+    // Evaluate if a device with the requested id exists
+    const reqDevice = db.get('devices').find({ id : req.params.id.toUpperCase() }).value();
+
+    if (reqDevice) {
+        // Evaluates if the query 'power' exists
+        if (req.query.power) {
+            // Turning the device 'on' or 'off' depending on the value of 'power'
+            switch (req.query.power.toUpperCase()) {
+                case 'ON':
+                    db.get('devices').find({ id : req.params.id.toUpperCase() }).assign({ on : true, state : 'on' }).value();
+                    break;
+                case 'OFF':
+                    db.get('devices').find({ id : req.params.id.toUpperCase() }).assign({ on : false, state : 'off' }).value();
+                    break;
+            };
+        };
+
+        // Evaluates if the query 'color' exists and has a value that is 3 or 6 characters long
+        if (req.query.color && req.query.color.length === 3 || req.query.color && req.query.color.length === 6) {
+            // Setting the device to have the desired color
+            db.get('devices').find({ id : req.params.id.toUpperCase() }).assign({ color : `#${req.query.color}` }).value();
+        };
+        
+        // Evaluates if the query 'brightness' exists and has a value that is between 0 and 1
+        if (req.query.brightness && Number(req.query.brightness) >= 0 && Number(req.query.brightness) <= 1) {
+            // Setting the device to have the desired brightness
+            db.get('devices').find({ id : req.params.id.toUpperCase() }).assign({ brightness : Number(req.query.brightness) }).value();
+        };
+
+        // Updating frontend
+        update();
+
+        // Sending the requested device-object as response
+        res.send(reqDevice);
+    }
+    else {
+        // Sending a response letting the user know no device with that ID can be found
+        res.send(`Cant find a device with that ID`);
+    };
+});
+
 // TODO: ADD ERROR HANDLING/RESPONSE IF REQUEST IS SENT TO A PATH THAT DOES NOT EXIST
 /*
 app.get('*', (req, res) => {
